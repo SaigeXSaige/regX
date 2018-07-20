913b14362fc6df0323eab00fa2ccc5a07b331a31
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-20 09:54:26 -0400

    changes navbar to only contain navigation icons and adds styling

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ class App extends Component {
           <img src={lightning} className="App-logo" alt="logo" />
           <img src={book} className="App-logo" alt="logo" />
           <img src={feedback} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
+        <h2>Welcome to RegX</h2>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
