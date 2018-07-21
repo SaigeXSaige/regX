6e4992a567c774952db1e6cdea508cd01989513f
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-20 20:52:29 -0400

    wordBank.js should be WordBank.js... strange

--- src/components/wordBank.js
+++ src/components/wordBank.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const WordBank = ({textArea}) => {
     return (
-        <div>
-            {textArea}
-        </div>
+        <form>
+            <textarea name="" id="" cols="30" rows="10" value={textArea}/>
+        </form>
     );
 }
 
