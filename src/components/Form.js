bc52ad08dadac04535d5eed5d821b04129c1bb20
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-20 20:12:19 -0400

    adds a component for creating form

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Form = (props) => {
+
+    return (
+        <form className="regex-input" >
+          <input type="text" name="input" id="input" />  
+        </form>
+    )
+};
+
+export default Form;
