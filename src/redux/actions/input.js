993bf59c8cd4fd4af0d03de0e5cf5b37d20b7272
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-22 18:44:38 -0400

    adds a new action to handle input changes to the word bank

--- src/redux/actions/input.js
+++ src/redux/actions/input.js
@@ -1,8 +1,14 @@
 //Action Types
 export const NEW_INPUT = 'NEW_INPUT'
+export const NEW_WORD_BANK = 'NEW_WORD_BANK'
 
 //Action Creators
 export const changeInput = (input) => ({
     type: NEW_INPUT,
     input: input
-}); //to change home page input 
+}); 
+
+export const changeWordBank = (input) => ({
+    type: NEW_WORD_BANK,
+    textarea: input
+})
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import { NEW_INPUT } from "../actions/input";
+import { NEW_INPUT, NEW_WORD_BANK } from "../actions/input";
 
 export const initialState = {
     textarea: "abcdefghifklmnopqrstuvwxyz1234567890!@#$%^&*()_+-=[]\\{}|;':\",./<>?`~",
@@ -10,6 +10,8 @@ export function inputChange(state = initialState, action) {
     switch (action.type) {
         case NEW_INPUT:
             return { ...state, input: action.input};
+        case NEW_WORD_BANK: 
+            return { ...state, textarea: action.textarea}
         default:
             return {...state};
     }
