4f6d1680f803064cf42083c3fbe275c7c2597b04
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-22 17:11:56 -0400

    adds highlighting functionality to reflect regex matches

--- src/components/WordBankContainer.js
+++ src/components/WordBankContainer.js
@@ -4,13 +4,25 @@ import { connect } from 'react-redux';
 import WordBank from "./WordBank";
 
 class WordBankContainer extends Component {
+    componentDidMount(){
+        this.highlightWords(this.props.textarea)
+    }
+
+    componentDidUpdate(){
+        this.highlightWords(this.props.textarea)
+    }
+ 
+    highlightWords = (str) => {
+        const expression = this.props.input.split("/")
+        const regex = new RegExp(expression[1],expression[2])
+        document.querySelector('.highlights').innerHTML = str.replace(regex, (match) => `<mark>${match}</mark>`)
+    }
+
     render() {
         return (
             <div className="container">
                 <div className="backdrop">
-                    <div className="highlights">
-                        {/* cloned text with <mark> tags here */}
-                    </div>
+                    <div className="highlights" />
                 </div>
                 < WordBank textArea={this.props.textarea} />
             </div>
@@ -18,4 +30,15 @@ class WordBankContainer extends Component {
     }
 }
 
-export default connect((state)=> ({textarea: state.textarea, input: state.input}))(WordBankContainer);
+const mapStateToProps = state => ({
+    ...state,
+    textarea: state.textarea
+})
+  
+const mapDispatchToState = dispatch => {
+    return {
+      handleWordBankChange: () => dispatch()
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToState)(WordBankContainer);
