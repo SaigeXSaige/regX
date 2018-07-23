8717a370774bf802729015974f14c828fe3dacab
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-23 14:02:00 -0400

    adds action to handle input submission

--- src/redux/actions/input.js
+++ src/redux/actions/input.js
@@ -1,6 +1,7 @@
 //Action Types
 export const NEW_INPUT = 'NEW_INPUT'
 export const NEW_WORD_BANK = 'NEW_WORD_BANK'
+export const SUBMIT_INPUT = 'SUBMIT_INPUT'
 
 //Action Creators
 export const changeInput = (input) => ({
@@ -11,4 +12,9 @@ export const changeInput = (input) => ({
 export const changeWordBank = (input) => ({
     type: NEW_WORD_BANK,
     textarea: input
+})
+
+export const submitInput = (bool) => ({
+    type: SUBMIT_INPUT,
+    answer: bool
 })
