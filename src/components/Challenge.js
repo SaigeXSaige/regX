2e6775e08807b13508b7be5b034e7aa497b2e3be
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-23 11:56:41 -0400

    creates a stateless component to render that level's challenge

---
+++ src/components/Challenge.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Challenge = (props) => {
+    return (
+        <div className="challenge">
+            This will be some challenge
+        </div>
+    );
+}
+
+export default Challenge;
