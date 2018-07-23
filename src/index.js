59eb015546eac4f0b46bfab1abd181be9a71f09d
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-23 11:03:29 -0400

    nests App inside Router component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
@@ -13,8 +13,8 @@ const store = configureStore();
 ReactDOM.render(
   <Provider store={store} >
     <Router>
-      <App />
-    </Router> 
+        <App />
+    </Router>
   </Provider >,
   document.getElementById('root')
 );
