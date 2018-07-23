9f6570ac11a89610fe690d45a38e7fc2d742c912
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-23 11:08:20 -0400

    adds navlinks to icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 
 import basicHome from "../icons/basicHome.svg";
 import lightning from "../icons/lightning.svg";
@@ -8,10 +9,18 @@ import feedback from "../icons/feedback.svg";
 const NavBar = (props) => {
     return (
         <div className="App-header">
-            <img src={basicHome} className="App-logo" alt="logo" />
-            <img src={lightning} className="App-logo" alt="logo" />
-            <img src={book} className="App-logo" alt="logo" />
-            <img src={feedback} className="App-logo" alt="logo" />
+            <NavLink to="/">
+                <img src={basicHome} className="App-logo" alt="logo" />
+            </NavLink>
+            <NavLink to="/challenges">
+                <img src={lightning} className="App-logo" alt="logo" />
+            </NavLink>
+            <NavLink to="/reference">
+                <img src={book} className="App-logo" alt="logo" />
+            </NavLink>
+            <NavLink to="/feedback">
+                <img src={feedback} className="App-logo" alt="logo" />
+            </NavLink>
         </div>
     );
 }
