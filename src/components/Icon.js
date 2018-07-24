6ff0581923b44633f47dfa4f72ca178195094c3d
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-24 16:10:10 -0400

    adds a class to represent which unicode character the icon represent

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const Icon = ({src, alt}) => {
-    return (
-        <img src={src} alt={alt} className="challenge-icon animated infinite bounce" />
-    );
+class Icon extends React.Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+    componentWillUnmount() {
+        console.log('unmount')
+    }
+
+    render( ) {
+        return (
+            <img src={this.props.src} alt={this.props.alt} className={`challenge-icon char-${this.props.alt} animated infinite bounce`} />
+        );
+
+    }
 }
 
 export default Icon;
