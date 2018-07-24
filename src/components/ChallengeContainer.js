83357a3e7c1627f473a30ba57219da1955fa6267
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-24 00:30:34 -0400

    changes props to know the level, challenge, and answer

--- src/components/ChallengeContainer.js
+++ src/components/ChallengeContainer.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 import ChallengePage from './ChallengePage';
 
 class ChallengeContainer extends Component {
+
     render() {
         return (
             <div className="App">
-                < ChallengePage level={1}/>
+                < ChallengePage level={1} challenge="Tak3 Out Num6ers" answer="3,6" />
                 {/* 
                 < ChallengePage level={2}/>
                 < ChallengePage level={3}/>
