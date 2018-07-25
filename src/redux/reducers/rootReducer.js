c0fdadd29e10f1961d3679c53bb8cfe8daa0dbb7
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-25 13:29:52 -0400

    adds the 3rd level and more icon imports

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -2,20 +2,31 @@ import { combineReducers } from "redux";
 import { NEW_INPUT, NEW_WORD_BANK, SUBMIT_INPUT } from "../actions/input";
 
 import a from '../../icons/lowercase/a.svg';
+import A from '../../icons/uppercase/A.svg';
 import b from '../../icons/lowercase/b.svg';
+import c from '../../icons/lowercase/c.svg';
+import C from '../../icons/uppercase/C.svg';
 import d from '../../icons/lowercase/d.svg';
+import D from '../../icons/uppercase/D.svg';
 import e from '../../icons/lowercase/e.svg';
 import G from '../../icons/uppercase/G.svg';
 import k from '../../icons/lowercase/k.svg';
+import l from '../../icons/lowercase/l.svg';
 import m from '../../icons/lowercase/m.svg';
 import M from '../../icons/uppercase/M.svg';
+import n from '../../icons/lowercase/n.svg';
 import N from '../../icons/uppercase/N.svg';
+import o from '../../icons/lowercase/o.svg';
 import O from '../../icons/uppercase/O.svg';
+import p from '../../icons/lowercase/p.svg';
+import P from '../../icons/uppercase/P.svg';
 import r from '../../icons/lowercase/r.svg';
 import s from '../../icons/lowercase/s.svg';
 import t from '../../icons/lowercase/t.svg';
 import T from '../../icons/uppercase/T.svg';
 import u from '../../icons/lowercase/u.svg';
+import y from '../../icons/lowercase/y.svg';
+import One from '../../icons/numbers/1.svg';
 import Three from '../../icons/numbers/3.svg';
 import Six from '../../icons/numbers/6.svg';
 import exclamation from '../../icons/special_chars/xclamation.svg'
@@ -37,6 +48,12 @@ export const initialState = {
             answer: ["Me!"],
             icons: [G,r,a,b,[M,b],[e,e],[exclamation,d]],
             specialChar: ["!", "x"]
+        },
+        "3": {
+            level: 3,
+            challenge: "On1y Caps And Num",
+            answer: ["O",1,"C","A","N"],
+            icons: [[O,o],n,[One,l],y,[C,c],a,p,s,[A,a],n,d,[N,n],u,m]
         }
     }
 }
