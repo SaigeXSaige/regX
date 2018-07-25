e0e5ebb3c2e03f1e91101b7398ae4a5193575515
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-25 00:36:32 -0400

    changes id to an iterative count to fix repeating letters not rendering more than once

--- src/components/Challenge.js
+++ src/components/Challenge.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Icon from './Icon'
 
 const Challenge = ({icons, correctInput}) => {
+    let id = 0
+
     return (
         <div className="challenge">
             {icons.map(icon => {
-                const id = icons.indexOf(icon)
+                id++
                 if (Array.isArray(icon)) {
-                    return correctInput ? <Icon key={icon + "1"} src={icon[1]} alt={icon[1].charAt(14)}/> : <Icon key={icon + "0"} src={icon[0]} alt={icon[0].charAt(14)}/>
+                    return correctInput ? <Icon key={id + "1"} src={icon[1]} alt={icon[1].charAt(14)}/> : <Icon key={id + "0"} src={icon[0]} alt={icon[0].charAt(14)}/>
                 } else {
                     return <Icon key={id} src={icon} alt={icon.charAt(14)}/>}
                 })
