af6215de6b2c2d6aaddf074d628f827270d282e9
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-25 00:29:37 -0400

    add keys to components so the same letter can be treated differently

--- src/components/Challenge.js
+++ src/components/Challenge.js
@@ -6,10 +6,11 @@ const Challenge = ({icons, correctInput}) => {
     return (
         <div className="challenge">
             {icons.map(icon => {
+                const id = icons.indexOf(icon)
                 if (Array.isArray(icon)) {
-                    return correctInput ? <Icon src={icon[1]} alt={icon[1].charAt(14)}/> : <Icon src={icon[0]} alt={icon[0].charAt(14)}/>
+                    return correctInput ? <Icon key={icon + "1"} src={icon[1]} alt={icon[1].charAt(14)}/> : <Icon key={icon + "0"} src={icon[0]} alt={icon[0].charAt(14)}/>
                 } else {
-                    return <Icon src={icon} alt={icon.charAt(14)}/>}
+                    return <Icon key={id} src={icon} alt={icon.charAt(14)}/>}
                 })
             }
         </div>    
