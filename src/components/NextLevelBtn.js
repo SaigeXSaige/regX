135a2abeab2097764b39a4ad90cbcd1d8d426e97
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-25 11:22:01 -0400

    refactors button to link to next level and update correctInput state back to false

--- src/components/NextLevelBtn.js
+++ src/components/NextLevelBtn.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { submitInput } from '../redux/actions/input';
 
-const NextLevelBtn = ({level}) => {
+const NextLevelBtn = ({level, handleClick}) => {
     return (
         <div>
-            {level === 2 ? <button disabled="disabled">Congratulations!</button> : <button>Next Level</button>}
+            {
+                level === 2 ? 
+                <button disabled="disabled">Congratulations!</button> : 
+                <Link to={`/challenges/${++level}`}>
+                    <button onClick={handleClick}>Next Level</button>
+                </Link>
+            }
         </div>
     );
 }
 
-export default NextLevelBtn;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handleClick: () => dispatch(submitInput(false))
+    } 
+};
+
+export default connect(null, mapDispatchToProps)(NextLevelBtn);
