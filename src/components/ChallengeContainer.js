b935802004e0233b11b99ba4614924dc9c39a6b4
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-24 23:03:59 -0400

    reorganize icon imports and add level 2 ChallengePage as a comment

--- src/components/ChallengeContainer.js
+++ src/components/ChallengeContainer.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react';
 
 import ChallengePage from './ChallengePage';
 
-import T from '../icons/uppercase/T.svg';
 import a from '../icons/lowercase/a.svg';
-import k from '../icons/lowercase/k.svg';
-import Three from '../icons/numbers/3.svg';
-import O from '../icons/uppercase/O.svg';
-import u from '../icons/lowercase/u.svg';
-import t from '../icons/lowercase/t.svg';
-import N from '../icons/uppercase/N.svg';
-import m from '../icons/lowercase/m.svg';
 import b from '../icons/lowercase/b.svg';
-import Six from '../icons/numbers/6.svg';
 import e from '../icons/lowercase/e.svg';
+import k from '../icons/lowercase/k.svg';
+import m from '../icons/lowercase/m.svg';
+import N from '../icons/uppercase/N.svg';
+import O from '../icons/uppercase/O.svg';
 import r from '../icons/lowercase/r.svg';
 import s from '../icons/lowercase/s.svg';
+import t from '../icons/lowercase/t.svg';
+import T from '../icons/uppercase/T.svg';
+import u from '../icons/lowercase/u.svg';
+import Three from '../icons/numbers/3.svg';
+import Six from '../icons/numbers/6.svg';
+import exclamation from '../icons/special_chars/!.svg'
 
 class ChallengeContainer extends Component {
 
@@ -26,10 +27,15 @@ class ChallengeContainer extends Component {
                     level={1} 
                     challenge="Tak3 Out Num6ers" 
                     answer={[3,6]} 
-                    icons={[T,a,k,[Three,e],O,u,t,N,u,m,[Six,b],e,r,s]}/>
-                {/* 
-                < ChallengePage level={2}/>
-                < ChallengePage level={3}/>
+                    icons={[T,a,k,[Three,e],O,u,t,N,u,m,[Six,b],e,r,s]}
+                />
+                {/* < ChallengePage 
+                    level={2} 
+                    challenge="Grab Me!"
+                    answer={[M,e,exclamation]}
+                    icons={[G,r,a,b,[M,b],[e,e],[exclamation,d]]}
+                /> */}
+                {/* < ChallengePage level={3}/>
                 < ChallengePage level={4}/>
                 < ChallengePage level={5}/> */}
             </div>
