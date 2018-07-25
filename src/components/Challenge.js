229298595bf24a33fc7b840997b85310b082e9ac
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-25 13:29:21 -0400

    changes how keys are rendered because 2 digit keys can be accidentally repeatedly

--- src/components/Challenge.js
+++ src/components/Challenge.js
@@ -9,8 +9,9 @@ const Challenge = ({icons, correctInput}) => {
         <div className="challenge">
             {icons.map(icon => {
                 id++
+
                 if (Array.isArray(icon)) {
-                    return correctInput ? <Icon key={id + "1"} src={icon[1]} alt={icon[1].charAt(14)}/> : <Icon key={id + "0"} src={icon[0]} alt={icon[0].charAt(14)}/>
+                    return correctInput ? <Icon key={"00"+ id} src={icon[1]} alt={icon[1].charAt(14)}/> : <Icon key={"01"+id} src={icon[0]} alt={icon[0].charAt(14)}/>
                 } else {
                     return <Icon key={id} src={icon} alt={icon.charAt(14)}/>}
                 })
