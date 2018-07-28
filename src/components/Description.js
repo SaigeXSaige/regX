5d11e8476eb03c3d887aaf35b91d733368c14f54
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-27 21:09:57 -0400

    changes class to footer

--- src/components/Description.js
+++ src/components/Description.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Description = ({description, link}) => {
     return (
-        <div className="footer card">
+        <div className="footer">
             <p>
                 {description}
                 {link && <a href="`https://en.wikipedia.org/wiki/List_of_Unicode_characters`" target="_blank">{link}</a> }
