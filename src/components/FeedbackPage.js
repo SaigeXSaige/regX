dae3db2240c33d210a0917729da1e97f276d82b7
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-29 16:34:28 -0400

    adds a state callback function to conditionally render form when feedback isnt submitted

--- src/components/FeedbackPage.js
+++ src/components/FeedbackPage.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import FeedbackForm from './FeedbackForm';
 
-const FeedbackPage = () => {
+const FeedbackPage = ({isSubmitted}) => {
     return (
         <div className="App">
+            { isSubmitted ?
+                <h2>Thank You For Helping Improve RegX!</h2> :
                 < FeedbackForm />
+            }
         </div>
     );
 }
 
-export default FeedbackPage;
+const mapStateToProps = (state) => ({
+    isSubmitted: state.isSubmitted
+})
+
+export default connect(mapStateToProps)(FeedbackPage);
