8e2fd56e447579ad8edd40df669a5744d78c1ea0
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-29 16:33:36 -0400

    adds a dispatch callback to handle submitting of user feedback

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const FeedbackForm = () => {
+import { submitFeedback } from '../redux/actions/input';
+
+const FeedbackForm = ({handleSubmit}) => {
     return (
         <div>
             <h2>Submit Your Feedback Below</h2>
-            <form className="feedback">
+            <form className="feedback" onSubmit={handleSubmit}>
                 <input type="text" id="email" placeholder="Email Address"/>
                 <textarea cols="30" rows="10" id="content" placeholder="Enter your feedback here"></textarea>
                 <input type="submit" value="Submit" id="submit" />
@@ -13,4 +16,11 @@ const FeedbackForm = () => {
     );
 }
 
-export default FeedbackForm;
+const mapDispatchToProps = (dispatch) => ({
+    handleSubmit: (e) => {
+        e.preventDefault();
+        dispatch(submitFeedback(true))
+    }
+})
+
+export default connect(null, mapDispatchToProps)(FeedbackForm);
