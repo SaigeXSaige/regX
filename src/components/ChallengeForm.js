d5ed0d6c7ede059a03dad8ebde5cbf746b9f8499
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-08-02 10:50:30 -0400

    makes code more readable with clearer variable names and more comments

--- src/components/ChallengeForm.js
+++ src/components/ChallengeForm.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { submitInput } from '../redux/actions/input';
 
-const ChallengeForm = ({challenge, answer, specialChar, handleSubmit}) => {
+const ChallengeForm = ({challenge, matches, specialChar, handleSubmit}) => {
     return (
-        <form className="regex-input" onSubmit={(e) => handleSubmit(e, challenge, answer, specialChar)}>
+        <form className="regex-input" onSubmit={(e) => handleSubmit(e, challenge, matches, specialChar)}>
             <input type="text" placeholder="Type expressions here" />
         </form>
     );
@@ -12,16 +12,15 @@ const ChallengeForm = ({challenge, answer, specialChar, handleSubmit}) => {
 
 const shakeIcons = () => {
     const iconList = document.querySelectorAll(".challenge-icon")
-    window.navigator.vibrate(200);         
+    window.navigator.vibrate(500);  //adds vibration to supported mobile devices      
     iconList.forEach( icon => {
         icon.classList.add("shake")
         setTimeout(() => icon.classList.remove("shake"), 500);
     })     
 }
 
-
 const mapDispatchToProps = (dispatch) => ({
-    handleSubmit: (e, challenge, answer, specialChar) => {
+    handleSubmit: (e, challenge, matches, specialChar) => {
         e.preventDefault()
 
         const userInput = e.target.firstChild.value
@@ -31,28 +30,28 @@ const mapDispatchToProps = (dispatch) => ({
             //splits user input into expression and flags
             const expression = userInput.split("/") 
             const regex = new RegExp(expression[1],expression[2]) 
-            //checks if the regex produced by the userInput would match the correct chars
+            //checks if the regex produced by the userInput would match the correct characters
             const result = challenge.match(regex) 
-            const bool = result ? result.join() === answer.join() : false 
-            // console.log('result:', result.join(), 'answer:', answer.join())
+            const bool = result ? result.join() === matches.join() : false 
             
             if (bool) {
-                if (answer.length === 1) {
-                    const str = answer.join()
+                if (matches.length === 1) {
+                    const str = matches.join()
                     //handles special characters that cant be written as a file name like "!"
                     for (let i = 0; i < str.length; i++) {
                         let y = str.charAt(i) === specialChar[0] ? specialChar[1] : str.charAt(i)
                         document.querySelector(`.char-${y}`).className = "challenge-icon animated flipOutX"
                     } 
                 } else {
-                    answer.forEach( 
-                        x => {
-                            if (x.length > 1) {
-                                for (let i = 0; i < x.length; i++) {
-                                    document.querySelector(`.char-${x.charAt(i)}`).className = "challenge-icon animated flipOutX"
+                    matches.forEach( 
+                        match => {
+                            //checks if match passed from state is more than one character and then iterates
+                            if (match.length > 1) {
+                                for (let i = 0; i < match.length; i++) {
+                                    document.querySelector(`.char-${match.charAt(i)}`).className = "challenge-icon animated flipOutX"
                                 } 
                             } else {
-                                document.querySelector(`.char-${x}`).className = "challenge-icon animated flipOutX"
+                                document.querySelector(`.char-${match}`).className = "challenge-icon animated flipOutX"
                             }
                         }
                     )
